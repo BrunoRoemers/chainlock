1fe3dd4b062735bf62b8b71e6cc04a4a030709c2
Bruno Roemers <bruno@roemers.io>
2022-06-05 18:13:20 +0200

    Add disclaimer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,13 @@ import Research from './research/Research';
 
 function App() {
   return (
-    <Research/>
+    <div>
+      <div className='bg-black text-white p-2 text-center'>
+        <h1>This dApp is under development - use at your own risk!</h1>
+        <p className='text-gray-500'>If you want to try out chainlock, create a new address in metamask and only use it on this website.</p>
+      </div>
+      <Research/>
+    </div>
   );
 }
 
