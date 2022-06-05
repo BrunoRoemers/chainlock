a78813d1ac49903642efcc21db675f136c1784fb
Bruno Roemers <bruno@roemers.io>
2022-06-05 22:23:11 +0200

    Fix App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders disclaimer', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This dApp is under development - use at your own risk!/i);
   expect(linkElement).toBeInTheDocument();
 });
