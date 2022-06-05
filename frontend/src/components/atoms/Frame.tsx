f7296427fbf5ae74aea670ccc897dc38c0e8effb
Bruno Roemers <bruno@roemers.io>
2022-06-06 01:58:53 +0200

    Fix css bug

--- frontend/src/components/atoms/Frame.tsx
+++ frontend/src/components/atoms/Frame.tsx
@@ -5,7 +5,7 @@ interface Props {
 const Frame = ({children}: Props) => {
   return (
     <div className="h-full flex justify-center items-center p-4">
-      <div className="grow max-w-md bg-pink-100 rounded-lg p-4">
+      <div className="grow max-w-full sm:max-w-md bg-pink-100 rounded-lg p-4">
         {children}
       </div>
     </div>
